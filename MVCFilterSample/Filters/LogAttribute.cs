87f42aa3a7de7a491d5f24440f127e3991a558a7
Ricardo Artola <rartola@outlook.com>
2021-03-02 13:04:35 -0500

    Add project files.

---
+++ MVCFilterSample/Filters/LogAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFilterSample.Filters
+{
+    public class LogAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            string controller = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            string action = filterContext.ActionDescriptor.ActionName;
+            string ip = filterContext.HttpContext.Request.UserHostAddress;
+            DateTime date = filterContext.HttpContext.Timestamp;
+
+            string path = filterContext.HttpContext.Server.MapPath("~/Log/log.txt");
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine();
+                sw.Write("Controller:" + controller);
+                sw.Write(",Action:" + action);
+                sw.Write(",IP:" + ip);
+                sw.Write(",Date:" + date.ToLongDateString());
+            }
+        }
+    }
+}
---
+++ MVCFilterSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCFilterSample.MvcApplication" Language="C#" %>
